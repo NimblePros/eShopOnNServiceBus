c00f8d93ae0f4157818e86c5d392b66f0454ed31
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2025-07-17 22:07:18 -0400

    Adding the install script to set up the queues

--- src/eShopOnWeb.Worker/Program.cs
+++ src/eShopOnWeb.Worker/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.Text.Json;
+using Microsoft.AspNetCore.Server.Kestrel;
 using Microsoft.eShopWeb.ApplicationCore.Configuration;
 using NServiceBus;
+using NServiceBus.Installation;
 
 var builder = Host.CreateDefaultBuilder();
 
 builder.UseConsoleLifetime();
 builder.UseNServiceBus(context => NServiceBusConfiguration.GetNServiceBusConfiguration());
+await Installer.Setup(NServiceBusConfiguration.GetNServiceBusConfiguration());
 
 var host = builder.Build();
 
